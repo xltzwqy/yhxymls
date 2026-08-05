ad56be82a69176bd23189c683b67cbdbe79172e3
xltzwqy <124780187+xltzwqy@users.noreply.github.com>
2026-08-05 10:21:42 +0800

    Make personal panel installable as a PWA

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'personal-panel-v1';
+const CORE_FILES = ['./', './index.html', './manifest.webmanifest', './icons/app-icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok || response.type === 'opaque') caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : cached)));
+});
